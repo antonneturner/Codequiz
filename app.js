56bb1d276788df736784fafe5f518f076a91bf72
Antonne Turner <antonneturner@gmail.com>
2020-11-14 21:11:26 -0500

    add questions, next question, select answer

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-//  Document variables
+// //  Document variables
 
-document.addEventListener("DOMContentLoaded", () => {
-  const timeLeftDisplay = document.querySelector("#time-left");
-  const startBtn = document.querySelector("#start-button");
-  let timeLeft = 10;
+// document.addEventListener("DOMContentLoaded", () => {
+//   const timeLeftDisplay = document.querySelector("#time-left");
+//   const startBtn = document.querySelector("#start-button");
+//   let timeLeft = 10;
 
-  //   Function countdown
+//   //   Function countdown
 
-  function countDown() {
-    setInterval(function () {
-      if (timeLeft <= 0) {
-        clearInterval((timeLeft = 0));
-      }
-      timeLeftDisplay.innerHTML = timeLeft;
-      timeLeft -= 1;
-    }, 1000);
-  }
-  // start button
+//   function countDown() {
+//     setInterval(function () {
+//       if (timeLeft <= 0) {
+//         clearInterval((timeLeft = 0));
+//       }
+//       timeLeftDisplay.innerHTML = timeLeft;
+//       timeLeft -= 1;
+//     }, 1000);
+//   }
+//   // start button
 
-  startBtn.addEventListener("click", countDown);
-});
+//   startBtn.addEventListener("click", countDown);
+// });
